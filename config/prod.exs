36f40359c7425535b6d5c38be4bd0d9fff6ddd79
knmsyk <kounomasayuki@gmail.com>
2015-12-28 02:47:08 +0900

    Set prod config for heroku

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,10 @@ use Mix.Config
 # which you typically run after static files are built.
 config :cazoc, Cazoc.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  url: [scheme: "https", host: "cazpc.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -60,6 +62,12 @@ config :logger, level: :info
 #
 #     config :cazoc, Cazoc.Endpoint, root: "."
 
+# Configure your database
+config :cazoc, Cazoc.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: 20
+
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
