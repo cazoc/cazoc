a9396498405d8a7b28d68955811c5727a8180748
knmsyk <kounomasayuki@gmail.com>
2016-02-05 21:58:14 +0900

    Clean

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Cazoc.Mixfile do
   def project do
     [app: :cazoc,
      version: "0.0.1",
-     elixir: "~> 1.2.0",
+     elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
