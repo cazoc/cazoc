2cc33f3a2ea98980a3468b963eb7d44a9ad8b77c
knmsyk <kounomasayuki@gmail.com>
2016-03-09 21:59:52 +0900

    Clean github_controller

--- web/controllers/github_controller.ex
+++ web/controllers/github_controller.ex
@@ -25,23 +25,23 @@ defmodule Cazoc.GithubController do
 
   def import(conn, %{"family" => family_params}) do
     author = Session.current_author(conn)
-    result = with {:ok, repo} <- Git.clone([family_params["source"], family_params["path"]]),
-      repository = Repository.changeset(%Repository{}, family_params),
-      {:ok, repository} <- Repo.insert(repository),
-      family = Family.changeset(%Family{repository_id: repository.id, author_id: author.id}, family_params),
-      {:ok, family} <- Repo.insert(family),
-      do: {:ok, family}
+    result = with {:ok, _} <- Git.clone([family_params["source"], family_params["path"]]),
+    repository = Repository.changeset(%Repository{}, family_params),
+    {:ok, repository} <- Repo.insert(repository),
+    family = Family.changeset(%Family{repository_id: repository.id, author_id: author.id}, family_params),
+    {:ok, family} <- Repo.insert(family),
+    do: {:ok, family}
 
-    case result do
-      {:ok, family} ->
-        conn
-        |> put_flash(:info, "Repository imported successfully.")
-        |> render(conn, :import, family: family)
-      {:error, _} ->
-        conn
-        |> put_flash(:info, "Failed to import repository.")
-        |> redirect(to: github_path(conn, :index))
-    end
+  case result do
+    {:ok, family} ->
+      conn
+      |> put_flash(:info, "Repository imported successfully.")
+      |> render(conn, :import, family: family)
+    {:error, _} ->
+      conn
+      |> put_flash(:info, "Failed to import repository.")
+      |> redirect(to: github_path(conn, :index))
+  end
   end
 
   def sync(conn, %{"family" => family_params}) do
@@ -50,24 +50,24 @@ defmodule Cazoc.GithubController do
     family = if is_nil(family), do: %Family{author_id: author.id}, else: family
     family = Family.changeset(family, family_params)
     result = with {:ok, family} <- Repo.insert_or_update(family),
-      {:ok, articles, failed_paths} <- download_articles(author, family),
-      do: {:ok, articles, failed_paths}
+    {:ok, articles, failed_paths} <- download_articles(author, family),
+    do: {:ok, articles, failed_paths}
 
-    case result do
-      {:ok, articles, failed_paths} ->
-        message = if length(failed_paths) == 0 do
-            "Repository synced successfully."
-          else
-            "Failed synced following files:\n#{failed_paths |> Enum.join("\n")}"
-          end
-        conn
-        |> put_flash(:info, message)
-        |> redirect(to: my_article_path(conn, :index))
-      {:error, _} ->
-        conn
-        |> put_flash(:info, "Failed to sync repository.")
-        |> redirect(to: github_path(conn, :index))
-    end
+  case result do
+    {:ok, _, failed_paths} ->
+      message = if length(failed_paths) == 0 do
+          "Repository synced successfully."
+        else
+          "Failed synced following files:\n#{failed_paths |> Enum.join("\n")}"
+        end
+      conn
+      |> put_flash(:info, message)
+      |> redirect(to: my_article_path(conn, :index))
+    {:error, _} ->
+      conn
+      |> put_flash(:info, "Failed to sync repository.")
+      |> redirect(to: github_path(conn, :index))
+  end
   end
 
   def delete(conn, %{"id" => id}) do
@@ -87,7 +87,7 @@ defmodule Cazoc.GithubController do
     result = trees["tree"]
     |> Enum.filter(&(is_valid_file &1))
     |> Enum.filter(&(is_nil Repo.get_by(Article, family_id: family.id, path: &1["path"], sha: &1["sha"])))
-    |> Enum.map(&(download_article author, family, &1["path"], &1["sha"]))
+    |> Enum.map(&(download_article author, family, &1["path"]))
     articles = result
     |> Enum.filter(&(elem(&1, 0) == :ok))
     |> Enum.map(&(elem(&1, 1)))
@@ -98,7 +98,7 @@ defmodule Cazoc.GithubController do
     {:ok, articles, failed_paths}
   end
 
-  defp download_article(author, family, path, sha) do
+  defp download_article(author, family, path) do
     client = Tentacat.Client.new(%{access_token: Author.token_github(author)})
     content = Tentacat.Contents.find(author.name, family.name, path, client)
     if is_valid_content(content) do
@@ -122,7 +122,7 @@ defmodule Cazoc.GithubController do
 
   defp parse_title(body, path) do
     default = "Title"
-    pattern = title_pattern(path)
+    pattern = title_pattern path
     if pattern do
       captured = Regex.named_captures(pattern, body, capture: :first)
       if captured, do: captured["title"], else: default
@@ -131,7 +131,7 @@ defmodule Cazoc.GithubController do
     end
   end
 
-  defp title_matcher(path) do
+  defp title_pattern(path) do
     cond do
       path =~ ~r/.+\.org$/ ->
         ~r/#\+title: (?<title>[^\n]+)/i
