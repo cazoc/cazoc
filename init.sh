463ec85bd3352819cdb324d47ac8a0545e99fab5
knmsyk <kounomasayuki@gmail.com>
2016-04-16 16:46:37 +0900

    Add files for server setting

---
+++ init.sh
@@ -0,0 +1,5 @@
+wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb && sudo dpkg -i erlang-solutions_1.0_all.deb
+sudo apt-get update
+sudo apt-get install esl-erlang
+sudo apt-get install elixir
+sudo apt-get install nginx
---
+++ updateconf.sh
@@ -0,0 +1,3 @@
+sudo cp cazoc.conf /etc/init/
+sudo cp cazoc /etc/nginx/sites-available/
+sudo service nginx restart
