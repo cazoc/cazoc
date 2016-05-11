07c1528bf8658cb8370d759ba94850f76b009630
knmsyk <kounomasayuki@gmail.com>
2016-05-12 00:31:56 +0900

    Update 0.1.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Cazoc.Mixfile do
 
   def project do
     [app: :cazoc,
-     version: "0.0.2",
+     version: "0.1.0",
      elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
