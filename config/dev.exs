6dc1e94eccd478d4c35521a7870617a681693ba3
knmsyk <kounomasayuki@gmail.com>
2016-07-21 23:12:26 +0900

    Fix ecto_repos

--- config/dev.exs
+++ config/dev.exs
@@ -34,6 +34,7 @@ config :logger, :console, format: "[$level] $message\n"
 config :phoenix, :stacktrace_depth, 20
 
 # Configure your database
+config :cazoc, ecto_repos: [Cazoc.Repo]
 config :cazoc, Cazoc.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
